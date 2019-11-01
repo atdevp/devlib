33d657a7b617890df6995315690b665d4358f535
jiangleiyu <jiangleiyu@sohu-inc.com>
2019-11-01 19:02:40 +0800

    update file tool

--- file/file.go
+++ file/file.go
@@ -1,25 +1,88 @@
 package file
 
 import (
+	"errors"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
-// 判断是否为目录
-func IsDir(p string) bool {
-	fi, err := os.Stat(p)
+func Stat(name string) (os.FileInfo, error) {
+	return os.Stat(name)
+}
+
+func isExist(err error) bool {
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return false
+}
+
+func IsDir(name string) bool {
+	fi, err := Stat(name)
 	if err != nil {
 		return false
 	}
 	return fi.IsDir()
 }
 
-func IsFile(p string) bool {
-	if IsDir(p) {
+func IsFile(name string) bool {
+	if IsDir(name) {
 		return false
 	}
 	return true
 }
 
-func IsExists(filename string) bool {
-	os.Chmod
+func IsExist(name string) bool {
+	_, e := Stat(name)
+	if isExist(e) {
+		return true
+	}
+	return false
+}
+
+func IsExec(name string) bool {
+
+	fi, err := Stat(name)
+	if !isExist(err) {
+		return false
+	}
+
+	if fi.IsDir() {
+		return false
+	}
+
+	m := fi.Mode().String()
+	if strings.Index(m, "x") == 3 {
+		return true
+	}
+	return false
+
+}
+
+func Size(name string) (int64, error) {
+	fi, e := os.Stat(name)
+	if fi.IsDir() {
+		return 0, errors.New("invalid file type")
+	}
+
+	if !isExist(e) {
+		return 0, e
+	}
+	return fi.Size(), nil
+}
+
+func Create(name string) (*os.File, error) {
+	return os.Create(name)
+}
+
+func Delete(name string) error {
+	return os.Remove(name)
+}
+
+func Abspath(name string) (string, error) {
+	return filepath.Abs(filepath.Dir(name))
 }
